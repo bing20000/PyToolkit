b04604d6310f46f2358aa5399cb40da622141a7e
Bingo <zephor.com@gmail.com>
2014-07-14 17:19:33 +0800

    fix pep8 errors for data/randomstr.py

--- data/randomstr.py
+++ data/randomstr.py
@@ -1,11 +1,18 @@
 import string
 import random
 
-def str_generator(size=6, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+
+# a tool stript for random string as test data
+# input factors:
+# size, length, charset
+
+def str_generator(size=6,
+                  chars=string.ascii_uppercase + string.ascii_lowercase
+                  + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-if __name__=="__main__":
-    count  = 10
+if __name__ == "__main__":
+    size = 10
     length = 20
-    for i in  range(count):
+    for i in range(size):
         print str_generator(length)
